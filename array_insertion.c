04bf340e8dbb9f40576e6feb9d4640fc01145c19
SHAMS AHMAD <75977026+cbisam69@users.noreply.github.com>
2022-01-31 14:13:58 +0530

    Create array_insertion.c

---
+++ array_insertion.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{   int arr[100],i,n,ele,pos;
+    printf("enter the size of array\n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+     for(i=0;i<n;i++)
+     {
+         printf("array element insertion is %d\n",arr[i]);
+     }
+ 
+     printf("enter the elemnt to insert\n");
+     scanf("%d",&ele);
+     printf("enter position to insert the element\n");
+     scanf("%d",&pos);//pos=n-1
+     
+     for(i=n-1;i>=pos-1;i--)
+     {
+         arr[i+1]=arr[i];
+     }
+     arr[pos-1]=ele;
+    
+     for(i=0;i<=n;i++)
+     {
+         printf("array elelmnt after insertion is %d\n",arr[i]);
+     }
+     return 0;
+}
