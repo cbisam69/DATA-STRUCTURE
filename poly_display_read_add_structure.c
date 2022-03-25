616f6f79ad95f17895663e23c41dad37bd79688b
SHAMS AHMAD <75977026+cbisam69@users.noreply.github.com>
2022-03-26 01:54:15 +0530

    Update poly_display_read_add_structure.c

--- poly_display_read_add_structure.c
+++ poly_display_read_add_structure.c
@@ -47,3 +47,58 @@
 	printf("%d(x^%d)+",p[i].coeff,p[i].expo);
 	printf("%d(x^%d)",p[t-1].coeff,p[t-1].expo);
 }
+ int addPoly(struct poly p1[10],struct poly p2[10],int t1,int t2,struct poly p3[10])
+ {
+	int i,j,k;
+
+
+	i=0;
+	j=0;
+	k=0;
+
+	while(i<t1 && j<t2)
+	{
+		if(p1[i].expo==p2[j].expo)
+		{
+			p3[k].coeff=p1[i].coeff + p2[j].coeff;
+			p3[k].expo=p1[i].expo;
+
+			i++;
+			j++;
+			k++;
+		}
+		else if(p1[i].expo>p2[j].expo)
+		{
+			p3[k].coeff=p1[i].coeff;
+			p3[k].expo=p1[i].expo;
+			i++;
+			k++;
+		}
+		else
+		{
+			p3[k].coeff=p2[j].coeff;
+			p3[k].expo=p2[j].expo;
+			j++;
+			k++;
+		}
+	}
+
+	/* for rest over terms of polynomial 1 */
+	while(i<t1)
+	{
+		p3[k].coeff=p1[i].coeff;
+		p3[k].expo=p1[i].expo;
+		i++;
+		k++;
+	}
+	/* for rest over terms of polynomial 2 */
+	while(j<t2)
+	{
+		p3[k].coeff=p2[j].coeff;
+		p3[k].expo=p2[j].expo;
+		j++;
+		k++;
+	}
+
+	return(k); /* k is number of terms in resultant polynomial*/
+ }
